a3b708afcf3e853c57124583ad69d4b80d9bbcf6
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-09 18:05:37 -0700

    initial

---
+++ Employeeseed.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee (id, first_name, role_id, manager_id)
+VALUES
+  (1,'Reyna', 'Ramirez', 1, 1),
+  (2,'Rosy', 'Garcia', 2, 2),
+  (3,'Vero', 'Wolf', 3, 3);
+
+
+
---
+++ index.js
@@ -0,0 +1,49 @@
+// get the client
+const mysql = require('mysql2');
+ 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
+ 
+// simple query
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+ 
+// with placeholder
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+  ['Page', 45],
+  function(err, results) {
+    console.log(results);
+  }
+);
+
+
+
+
+
+
+
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
